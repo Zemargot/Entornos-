b36bfe4a1ede351c3174c2d9a876f7d503e7aef0
AntonioAguirreMunoz <57455191+AntonioAguirreMunoz@users.noreply.github.com>
2020-03-24 19:46:57 +0100

    Update Composicion.java

--- src/Antonio/Composicion.java
+++ src/Antonio/Composicion.java
@@ -1,4 +1,17 @@
 package Antonio;
 
+import static General.Triangulo.calcularArea;
+
 public class Composicion {
-}
+
+    public boolean calcularAreaMayorQue10(){
+        if (calcularArea() < 10)
+            return false;
+        return true;
+    }
+
+    public boolean calcularAreaMenorQur10(){
+        if (calcularArea() > 10)
+            return false;
+        return true;
+    }
